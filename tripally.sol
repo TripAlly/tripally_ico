7d28c72e9924834cb243e90527e4801386fec0ba
Andrey Zolin <giorkin@gmail.com>
2017-09-02 19:33:57 +0300

    change tokenPrice

--- tripally.sol
+++ tripally.sol
@@ -173,7 +173,7 @@ contract TripAlly is SafeMath, StandardToken, Pausable {
     uint256 public constant tokenCreationCap = 100000000*10**decimals;
     uint256 constant tokenCreationCapPreICO = 750000*10**decimals;
 
-    uint256 public oneTokenInWei = 3000000000000000;
+    uint256 public oneTokenInWei = 2000000000000000;
 
     uint public totalEthRecieved;
 
